7957c4771726de814f6ba9926a15d4af86bfe44e
J. Grimberg <github@joelgrimberg.nl>
2024-07-07 13:33:35 +0200

    create dedicated aliases.sh script

---
+++ scripts/aliases.sh
@@ -0,0 +1,32 @@
+alias ls='eza -lh --group-directories-first --icons --all'
+alias lsa='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
+alias ff="fzf --preview 'bat --theme=\"Catppuccin Mocha\" --color=always {}' --preview-window '~3'"
+alias cat='bat'
+
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# Tools
+alias n='nvim'
+alias g='git'
+alias d='docker'
+alias r='rails'
+alias lzg='lazygit'
+alias lzd='lazydocker'
+
+# Git
+alias gcm='git commit -m'
+alias gcam='git commit -a -m'
+alias gcad='git commit -a --amend'
+
+alias vi='nvim'
+
+alias rsync-copy='rsync -v -r --progress '
+
+# Compression
+compress() { tar -czf "${1%/}.tar.gz" "${1%/}"; }
+alias decompress="tar -xzf"
