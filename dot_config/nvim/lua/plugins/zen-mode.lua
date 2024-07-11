9213ec4ee52d635f099b2f3a04509b2f63931ae0
J. Grimberg <github@joelgrimberg.nl>
2024-07-11 14:53:55 +0200

    add zen-mode.lua

---
+++ dot_config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,30 @@
+return {
+	"folke/zen-mode.nvim",
+	cmd = "ZenMode",
+	init = function()
+		vim.keymap.set("n", "<Leader>zz", "<Cmd>ZenMode<CR>", { silent = true })
+		vim.keymap.set("n", "<Leader>zZ", function()
+			require("zen-mode").toggle({ window = { width = 0.5 } })
+		end, { silent = true, desc = "Toggle ZenMode (width=0.5)" })
+	end,
+	opts = {
+		window = {
+			backdrop = 1,
+			width = 0.7,
+			-- height = 0.95,
+			options = {
+				signcolumn = "no",
+				number = false,
+				relativenumber = false,
+				foldcolumn = "0",
+			},
+		},
+		plugins = {
+			options = {
+				enabled = true,
+				ruler = false,
+				showcmd = false,
+			},
+		},
+	},
+}
