e28f4c4a8b0bfce525c123132f0e4904af7c287e
J. Grimberg <github@joelgrimberg.nl>
2024-07-30 14:47:22 +0200

    iterate on installer

--- scripts/executable_installer.sh
+++ scripts/executable_installer.sh
@@ -9,19 +9,25 @@ else
   if [ -d "$HOME/.tmux/plugins/tpm" ]; then
     echo "TMUX Plugin Manager already installed"
   else
+    echo "installing TMUX Plugin Manager..."
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null 2>&1
+    echo "✅ TMUX Plugin Manager installed"
   fi
   
   if [ -d "$HOME/zsh-plugins/zsh-syntax-highlighting" ]; then
     echo "zsh-syntax-highlighting already installed"
   else
+    echo "installing zsh-syntax-highlighting..."
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/zsh-plugins/zsh-syntax-highlighting  > /dev/null 2>&1
+    echo "✅ zsh-syntax-highlighting installed"
   fi
   
   if [ -d "$HOME/zsh-plugins/zsh-autosuggestions" ]; then
     echo "zsh-auto-suggestions already installed"
   else
+    echo "installing zsh-autosuggestions..."
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/zsh-plugins/zsh-autosuggestions > /dev/null 2>&1
+    echo "✅ zsh-autosuggestions installed"
   fi
 
 fi
