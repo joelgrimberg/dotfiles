ff874d7ed8d7066d4bcf254119f5781df199b466
J. Grimberg <github@joelgrimberg.nl>
2024-12-22 13:08:38 +0100

    iterate on fish

--- dot_config/private_fish/alias.fish
+++ dot_config/private_fish/alias.fish
@@ -1,9 +1,9 @@
-alias vi     'nvim'
-alias ll     'eza -lh --group-directories-first --icons --all'
-alias ls     'eza -h --group-directories-first --icons --all'
-alias fish   'fzf --preview \'bat --color=always {}\' --preview-window \'~3\''
-alias cat    'bat'
-alias icat   'kitten icat'
+alias vl 'NVIM_APPNAME=nvim nvim'
+alias ll 'eza -lh --group-directories-first --icons --all'
+alias ls 'eza -h --group-directories-first --icons --all'
+alias fish 'fzf --preview \'bat --color=always {}\' --preview-window \'~3\''
+alias cat bat
+alias icat 'kitten icat'
 
-alias gpaf   'git add . && git commit --amend --no-edit && git push origin -f'
-alias vl     'NVIM_APPNAME=lazyvim vi'
+alias gpaf 'git add . && git commit --amend --no-edit && git push origin -f'
+alias vi 'NVIM_APPNAME=lazyvim nvim'
--- dot_config/private_fish/functions/vv.fish
+++ dot_config/private_fish/functions/vv.fish
@@ -1,7 +1,7 @@
 function vv --description "Open nvim with a specific config"
-    set -l configs nvim lazyvim 
+    set -l configs nvim lazyvim neovim neobean
     set -l selected_config (printf "%s\n" $configs | fzf)
-    
+
     if test -n "$selected_config"
         set -x NVIM_APPNAME $selected_config
         nvim $argv
