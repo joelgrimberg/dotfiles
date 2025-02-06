ce594b1bcbb9f9c11c202be35b9bbb72c4ed7fba
J. Grimberg <github@joelgrimberg.nl>
2025-02-06 16:04:09 +0100

    Update .config/fish/functions/vv.fish

--- dot_config/private_fish/functions/vv.fish
+++ dot_config/private_fish/functions/vv.fish
@@ -1,5 +1,5 @@
 function vv --description "Open nvim with a specific config"
-    set -l configs nvim lazyvim neovim neobean
+    set -l configs nvim lazyvim neovim neobean clean-lazyvim
     set -l selected_config (printf "%s\n" $configs | fzf)
 
     if test -n "$selected_config"
