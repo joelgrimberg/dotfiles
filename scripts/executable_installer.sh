a0c3d189dde4e191f2a7d2b2b0d1772d017f8abd
J. Grimberg <github@joelgrimberg.nl>
2025-03-06 09:28:00 +0100

    iteration

--- scripts/executable_installer.sh
+++ scripts/executable_installer.sh
@@ -4,6 +4,36 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 ENDCOLOR="\e[0m"
 
+# curl https://raw.githubusercontent.com/kentcdodds/dotfiles/HEAD/.macos | bash
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+###############################################################################
+# Joel's Customizations                                                       #
+###############################################################################
+
+echo "Hello $(whoami)! Let's get you set up."
+
+echo "mkdir -p ${HOME}/code"
+mkdir -p "${HOME}/code"
+
+echo "installing homebrew"
+# install homebrew https://brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install chezmoi
 
 if ! type git > /dev/null ; then
   echo -e "${RED}git is not installed ${ENDCOLOR}"
@@ -46,4 +76,29 @@ else
    wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
    bat cache --build
   fi
+
+  chezmoi init git@github.com:joelgrimberg/dotfiles.git
+  chezmoi apply
+
+  brew bundle install
+
+  echo "Generating a new SSH key for GitHub"
+  ssh-keygen -t ed25519 -C "me+github@joelgrimberg.nl" -f ~/.ssh/id_ed25519
+  eval "$(ssh-agent -s)"
+  touch ~/.ssh/config
+  echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+  ssh-add -K ~/.ssh/id_ed25519
+  echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
+
+  ## Login GH to create a ssh key
+  gh auth login
+  
+  ## clone fonts
+  git clone --depth=1 git@github.com:joelgrimberg/Berkeley-Mono-Fonts.git /Library/Fonts
+  rm -rf /Library/Fonts/.git
+
 fi
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
